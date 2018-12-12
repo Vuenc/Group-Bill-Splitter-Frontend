c6cba805734c64a0e5f7421332a43b51c0e213ce
vuenc <=v.buergin@gmx.de>
2018-12-12 16:48:55 +0000

    fixed server communication

--- src/services/groupbillsplitterservice.js
+++ src/services/groupbillsplitterservice.js
@@ -4,33 +4,33 @@ let jsonHeaders = {headers: {'Content-type': 'application/json'}}
 
 export default {
   fetchExpenses (groupEventId) {
-    return Api().get('/groupEvents/{$groupEventId}/expenses')
+    return Api().get(`/groupEvents/${groupEventId}/expenses`)
   },
   fetchMembers (groupEventId) {
-    return Api().get('/groupEvents/{$groupEventId}/members')
+    return Api().get(`/groupEvents/${groupEventId}/members`)
   },
   fetchGroupEvent (groupEventId) {
-    return Api().get('/groupEvents/{$groupEventId}')
+    return Api().get(`/groupEvents/${groupEventId}`)
   },
   fetchTransactions (groupEventId) {
-    return Api().get('/groupEvents/{$groupEventId}/transactions')
+    return Api().get(`/groupEvents/${groupEventId}/transactions`)
   },
   postGroupEvent (groupEvent) {
-    return Api().post('/groupEvents', groupEvent, jsonHeaders)
+    return Api().post(`/groupEvents`, groupEvent, jsonHeaders)
   },
   postGroupMember (groupEventId, member) {
-    return Api().post('/groupEvents/{$groupEventId}/members', member, jsonHeaders)
+    return Api().post(`/groupEvents/${groupEventId}/members`, member, jsonHeaders)
   },
   postExpense (groupEventId, expense) {
-    return Api().post('/groupEvents/{$groupEventId}/expenses', expense, jsonHeaders)
+    return Api().post(`/groupEvents/${groupEventId}/expenses`, expense, jsonHeaders)
   },
   putGroupEvent (groupEvent) {
-    return Api().put('/groupEvents/{$groupEvent._id}', groupEvent, jsonHeaders)
+    return Api().put(`/groupEvents/${groupEvent._id}`, groupEvent, jsonHeaders)
   },
   putGroupMember (groupEventId, member) {
-    return Api().put('/groupEvents/{$groupEventId}/members/{$member._id}', member, jsonHeaders)
+    return Api().put(`/groupEvents/${groupEventId}/members/${member._id}`, member, jsonHeaders)
   },
   putExpense (groupEventId, expense) {
-    return Api().put('/groupEvents/{$groupEventId}/expenses/{$expense._id}', expense, jsonHeaders)
+    return Api().put(`/groupEvents/${groupEventId}/expenses/${expense._id}`, expense, jsonHeaders)
   }
 }
