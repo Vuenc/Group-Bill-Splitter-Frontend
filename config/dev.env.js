750dc9a402a233f12a5a9dff2aab96a7e6d92729
vuenc <=v.buergin@gmx.de>
2018-12-17 01:17:46 +0000

    added backend link to production config

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  VUE_APP_BACKEND_URL: '"https://wat2-group-bill-splitter.herokuapp.com/"'
+  NODE_ENV: '"development"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  VUE_APP_BACKEND_URL: '"https://wat2-group-bill-splitter.herokuapp.com/"'
 }
