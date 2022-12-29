92151bd8e753537b3c5a5cacaedefb2f2c765cde
Vuenc <v.buergin@gmx.de>
2022-12-30 00:10:41 +0100

    Changed backend link to render deploy

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_BACKEND_URL: '"https://wat2-group-bill-splitter.herokuapp.com/"'
+  VUE_APP_BACKEND_URL: '"https://groupbillsplitter-backend.onrender.com/"'
 }
